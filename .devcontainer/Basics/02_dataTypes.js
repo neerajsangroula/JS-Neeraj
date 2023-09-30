f18e7b3b76813ae47ea64745763a2a80183edec9
neerajsangroula <59104888+neerajsangroula@users.noreply.github.com>
2023-09-30 18:35:03 +0000

    Datatypes

--- .devcontainer/Basics/02_dataTypes.js
+++ .devcontainer/Basics/02_dataTypes.js
@@ -1,6 +1,6 @@
+"use strict"; // treat all Js code as newer version
 
-
-// There are different types of data types 1) numbers 2)String -> "" 3) Boolean is True or False 4) null
+// There are different types of data types: 1) numbers 2)String -> "" 3) Boolean is True or False 4) null
 // Undefined -> 
 // symbol ->  unique
 
