6dc7a09d1eee528f7211db39f92fb573ab9a86a6
neerajsangroula <59104888+neerajsangroula@users.noreply.github.com>
2023-09-30 18:30:52 +0000

    Variables

--- .devcontainer/Basics/Variables.js
+++ .devcontainer/Basics/Variables.js
@@ -2,6 +2,6 @@ const accountId = '12345'
 var accountMail = 'n@gmail.com'
 let accountPw = '23456'
 
-// we should not use var now in java script because it causes problem with the scopes. scope is {when we use the function}
+// we should not use var now in java script because it causes problem with the block scopes and functional scope. scope is {when we use the function}
 
 console.table([accountId, accountMail, accountPw]);
