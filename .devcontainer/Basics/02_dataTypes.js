fd004c00855206581709d3069fea7dc6a2dd3b69
neerajsangroula <59104888+neerajsangroula@users.noreply.github.com>
2023-09-30 18:31:04 +0000

    Datatypes

---
+++ .devcontainer/Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+
+
+// There are different types of data types 1) numbers 2)String -> "" 3) Boolean is True or False 4) null
+// Undefined -> 
+// symbol ->  unique
+
+
+// object
+
+// null is an object
+
+let Name = "Neeraj"
+let age = 15
+
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // undefined
