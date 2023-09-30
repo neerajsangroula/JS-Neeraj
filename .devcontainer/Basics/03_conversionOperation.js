5b1df9becfc78cbd11d01da9ef3c196ac68a94c5
neerajsangroula <59104888+neerajsangroula@users.noreply.github.com>
2023-09-30 19:14:45 +0000

    Conversion of Data Types

---
+++ .devcontainer/Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+// "33"-> 33
+// "33abc" -> NaN
+
+
+let isLoggedIn = "Neeraj" // cpnverting number into booleam
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> True , 0=> False
+ // "" => False
+ // "neeraj"=> True
+
+ let someNumber = 33
+
+ let stringNumber = String (someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
