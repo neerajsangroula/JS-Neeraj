a47607407fb7b752b656b40c0c1f41e3c1487703
neerajsangroula <59104888+neerajsangroula@users.noreply.github.com>
2023-10-02 20:12:06 +0000

    Datatypes summary

---
+++ .devcontainer/Basics/Datatypes_summary.js
@@ -0,0 +1,19 @@
+// There are only two types of data types : primitive and non-primitive
+
+// Primitive (call by value)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+
+const name = "neeraj"
+const score = 100
+const isLoggedIn= true
+const outTemp = null
+let userEmail = undefined
+
+
+
+// Non-Primitive (Reference type)
+
+// Array , Objects, Function
